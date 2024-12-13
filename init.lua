881c9b50d4ccecea6978ad4377757582ee5e9862
alforslu <alex@forslund.dev>
2024-12-13 01:07:29 +0100

    Colorscheme commit

---
+++ init.lua
@@ -0,0 +1,46 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+-- Lazy.nvim:
+    require('lazy').setup({
+        {
+            "Mofiqul/adwaita.nvim",
+            lazy = false,
+            priority = 1000,
+            config = function()
+                vim.g.adwaita_darker = true
+                vim.g.adwaita_transparent = true
+                vim.cmd('colorscheme adwaita')
+            end
+        },
+    })
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
