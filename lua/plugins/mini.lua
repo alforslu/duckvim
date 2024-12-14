333c13e9e305d0cbd9356c6bc7465cf2c348fdcd
alforslu <alex@forslund.dev>
2024-12-14 15:28:39 +0100

    Add mini.nvim

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,24 @@
+return {
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+        -- Commenting
+        require("mini.comment").setup({
+            mappings = {
+                comment = "''",
+                comment_line = "<leader>/",
+                comment_visual = "<leader>/",
+                textobject = "<leader>/",
+            }
+        })
+
+        -- Bracket movement (Buffer, diagnostic, comment etc..)
+        require("mini.bracketed").setup()
+
+        -- Icons dep
+        require("mini.icons").setup()
+
+        -- Paired brackets
+        require("mini.pairs").setup()
+    end
+}
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,15 +2,26 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        spec = {
+            { '<leader>c', group = '[C]ode',     mode = { 'n', 'x' } },
+            { '<leader>d', group = '[D]ocument' },
+            { '<leader>r', group = '[R]ename' },
+            { '<leader>f', group = '[F]ind' },
+            { '<leader>w', group = '[W]orkspace' },
+            { '<leader>t', group = '[T]oggle' },
+            { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        }
     },
     keys = {
         {
             "<leader>?",
             function()
                 require("which-key").show({ global = false })
+
+                local wk = require("which-key")
+                wk.register({
+
+                })
             end,
             desc = "Buffer Local Keymaps (which-key)",
         },
