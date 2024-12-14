f293e4f33560e8281da79cf0eabfe364d907c647
alforslu <alex@forslund.dev>
2024-12-14 19:37:37 +0100

    Color line nr

--- lua/plugins/cyberdream.lua
+++ lua/plugins/cyberdream.lua
@@ -7,6 +7,13 @@ return {
             transparent = true,
             italic_comments = true,
             borderless_telescope = false,
+            theme = {
+                highlights = {
+                    LineNr = { fg = "#ff5ea0" },
+                    LineNrAbove = { fg = "#4f535a" },
+                    LineNrBelow = { fg = "#4f535a" },
+                },
+            }
         })
 
         vim.cmd("colorscheme cyberdream")
