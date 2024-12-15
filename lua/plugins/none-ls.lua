95e62ea920a4ac5415ab35d35083d2cb3ce3c9c3
alforslu <alex@forslund.dev>
2024-12-15 02:16:29 +0100

    Add autosave

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -3,6 +3,7 @@ return {
     "nvimtools/none-ls.nvim",
     config = function()
         local null_ls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
         null_ls.setup({
             sources = {
@@ -13,6 +14,20 @@ return {
                 null_ls.builtins.formatting.clang_format,
                 null_ls.builtins.diagnostics.eslint_d,
             },
+
+            -- Autosave on write
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format()
+                        end,
+                    })
+                end
+            end,
         })
     end,
 }
