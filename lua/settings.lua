d749f6fbeba42ab35595aab6f25eca72231d4314
alforslu <alex@forslund.dev>
2025-03-26 00:28:26 +0100

    Update, also enable virtual text

--- lua/settings.lua
+++ lua/settings.lua
@@ -86,3 +86,13 @@ vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Live changes in replace and such
 vim.opt.inccommand = "split"
+
+vim.diagnostic.config({
+    virtual_text = {
+        prefix = ">>",
+        spacing = 4,
+    },
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+})
