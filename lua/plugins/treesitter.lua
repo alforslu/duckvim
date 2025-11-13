6d8a1bb86b544cfd3df93a101671e276cc736aa1
alforslu <alex@forslund.dev>
2025-11-13 19:19:06 +0100

    Major update + Change treesitter to use new syntax

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,15 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    branch = "main",
     config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            -- ensure_installed = {"c", "cpp", "lua", "vim", "vimdoc"},
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
+        require("nvim-treesitter").install({ "c", "cpp", "lua", "vim", "vimdoc", "rust" })
+        -- local config = require("nvim-treesitter.configs")
+        -- config.setup({
+        --     -- ensure_installed = {"c", "cpp", "lua", "vim", "vimdoc"},
+        --     auto_install = true,
+        --     highlight = { enable = true },
+        --     indent = { enable = true },
+        -- })
     end,
 }
